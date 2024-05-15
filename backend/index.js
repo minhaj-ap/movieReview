b945f17f12b390acee68ef75ae97abd7b304d002
Minhaj Ap <minhajap00@gmail.com>
2024-05-15 12:20:06 +0530

    Movie data's are now receiving from TMDB

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-console.log("hello")
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from the React app's origin
+  })
+);
+// Define a route to fetch movies
+
+app.get("/movies", async (req, res) => {
+  try {
+    const apiKey = "d9fdf3dc87ba0c9297001381d0a1df80";
+    const { query } = req.query;
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=${apiKey}&query=${encodeURIComponent(
+        query
+      )}`
+    );
+    const movies = response.data.results;
+    movies.sort((a, b) => b.popularity - a.popularity);
+    res.json(movies);
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    res.status(500).json({ error: "Error fetching movies" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,6 +13,23 @@ export default function Header() {
   function handleMode() {
     setisDark((prev) => !prev);
   }
+  function searchFunc(e) {
+    e.preventDefault();
+    const searchTerm = document.getElementById("searchInput").value;
+    const baseUrl = "http://localhost:3001/movies"; // Your base URL
+    const url = `${baseUrl}?query=${encodeURIComponent(searchTerm)}`;
+
+    // Make request using fetch
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle response data
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   return (
@@ -21,8 +38,8 @@ export default function Header() {
         <p>CINEMA REVIEWER</p>
       </div>
       {search ? (
-        <form className="header_form">
-          <input type="text" placeholder="Search Movies..." />
+        <form className="header_form" onSubmit={searchFunc}>
+          <input type="text" id="searchInput" placeholder="Search Movies..." />
           <button type="submit">
             <Icon sx={{ color: "green" }}>
               <SearchIcon />

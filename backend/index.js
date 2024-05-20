f9f0516e1112aee592142916be0f3f917f75dc72
Minhaj Ap <minhajap00@gmail.com>
2024-05-21 00:04:13 +0530

    cache system implemented

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const axios = require("axios");
+const nodeCache = require("node-cache");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { connectDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
+const cache = new nodeCache({ stdTTL: 0, checkperiod: 60 });
 app.get("/search", async (req, res) => {
   try {
     const keyword = req.query.q;
@@ -34,13 +36,44 @@ app.get("/search", async (req, res) => {
 app.get("/top-movie", async (req, res) => {
   try {
     const db = getDb();
-    const ids = await db.collection("top5").find({}).toArray();
-    const id = ids[0].top5;
     const result = await db
-      .collection("movie_details")
-      .find({ id: { $in: id } })
+      .collection("top5")
+      .aggregate([
+        {
+          $match: { _id: new ObjectId("664b68750c2cefd8aa192f4d") },
+        },
+        {
+          $unwind: "$top5",
+        },
+        {
+          $lookup: {
+            from: "movie_details",
+            localField: "top5",
+            foreignField: "id",
+            as: "movieDetails",
+          },
+        },
+        {
+          $unwind: "$movieDetails",
+        },
+        {
+          $project: {
+            _id: 0,
+            movieId: "$top5",
+            title: "$movieDetails.title",
+            desc: "$movieDetails.desc",
+            genre_ids: "$movieDetails.genre_ids",
+          },
+        },
+      ])
       .toArray();
-    return res.json(result);
+    let cacheExpiry = 24 * 60 * 60;
+    if (result) {
+      cache.set("top5", result, cacheExpiry);
+    }
+    const cachedResults = cache.get("top5");
+    console.log(cache.getStats());
+    return res.json(cachedResults || result);
   } catch (error) {
     res.status(500).json({ error: "error fetching data" });
     console.error("Error fetching trending movies:", error);
@@ -54,8 +87,13 @@ app.get("/get-movies", async (req, res) => {
       .collection("movie_details")
       .find({ genre_ids: parseInt(query) })
       .toArray();
-    console.log(result);
-    res.json(result);
+    let cacheExpiry = 3 * 24 * 60 * 60;
+    if (result) {
+      cache.set(`movie${query}`, result, cacheExpiry);
+    }
+    console.log(cache.keys());
+    const cachedResults = cache.get(`movie${query}`);
+    return res.json(cachedResults || result);
   } catch (error) {
     res.status(500).json({ error: "Error fetching data" });
     console.log("error fetching data", error);

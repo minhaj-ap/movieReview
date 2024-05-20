a1b68bb00e8b78c1d7d3c6145ae1cd09d39901eb
Minhaj Ap <minhajap00@gmail.com>
2024-05-20 22:58:09 +0530

    fetching movies is now ready.

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,6 @@ const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
-// Define a route to fetch movies
-
-const apiKey = process.env.TMDB_API;
 app.get("/search", async (req, res) => {
   try {
     const keyword = req.query.q;
@@ -33,31 +30,17 @@ app.get("/search", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const { query } = req.query;
-  //   const response = await axios.get(
-  //     `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=${apiKey}&query=${encodeURIComponent(
-  //       query
-  //     )}`
-  //   );
-  //   const movies = response.data.results;
-  //   movies.sort((a, b) => b.popularity - a.popularity);
-  //   res.json(movies);
-  // } catch (error) {
-  //   console.error("Error fetching movies:", error);
-  //   res.status(500).json({ error: "Error fetching movies" });
-  // }
 });
 app.get("/top-movie", async (req, res) => {
-  const trendingUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
-  // Fetch trending movies
   try {
-    const response = await fetch(trendingUrl);
-    const data = await response.json();
-    // Get the top 5 trending movies
-    const top5TrendingMovies = data.results.slice(0, 5);
-    // console.log('Top 5 Trending Movies:', top5TrendingMovies);
-    res.json(top5TrendingMovies);
+    const db = getDb();
+    const ids = await db.collection("top5").find({}).toArray();
+    const id = ids[0].top5;
+    const result = await db
+      .collection("movie_details")
+      .find({ id: { $in: id } })
+      .toArray();
+    return res.json(result);
   } catch (error) {
     res.status(500).json({ error: "error fetching data" });
     console.error("Error fetching trending movies:", error);
@@ -66,10 +49,13 @@ app.get("/top-movie", async (req, res) => {
 app.get("/get-movies", async (req, res) => {
   try {
     const { query } = req.query;
-    const fetchingUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${query}&page=1`;
-    const response = await fetch(fetchingUrl);
-    const data = await response.json();
-    res.json(data);
+    const db = getDb();
+    const result = await db
+      .collection("movie_details")
+      .find({ genre_ids: parseInt(query) })
+      .toArray();
+    console.log(result);
+    res.json(result);
   } catch (error) {
     res.status(500).json({ error: "Error fetching data" });
     console.log("error fetching data", error);

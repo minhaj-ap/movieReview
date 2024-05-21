afcd50695edb7144d4c646f6bc3abb5945427758
Minhaj Ap <minhajap00@gmail.com>
2024-05-21 11:40:05 +0530

    Most of the functions are ready

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ app.get("/search", async (req, res) => {
     const movies = result.filter((movie) => {
       return regex.test(movie.title);
     });
-    console.log(movies);
     res.json(movies);
   } catch (error) {
+    res.status(500).send(error);
     console.log(error);
   }
 });
@@ -68,11 +68,10 @@ app.get("/top-movie", async (req, res) => {
       ])
       .toArray();
     let cacheExpiry = 24 * 60 * 60;
-    if (result) {
+    const cachedResults = cache.get("top5");
+    if (!cachedResults && result) {
       cache.set("top5", result, cacheExpiry);
     }
-    const cachedResults = cache.get("top5");
-    console.log(cache.getStats());
     return res.json(cachedResults || result);
   } catch (error) {
     res.status(500).json({ error: "error fetching data" });
@@ -88,15 +87,118 @@ app.get("/get-movies", async (req, res) => {
       .find({ genre_ids: parseInt(query) })
       .toArray();
     let cacheExpiry = 3 * 24 * 60 * 60;
-    if (result) {
+    const cachedResults = cache.get(`movie${query}`);
+    if (!cachedResults && result) {
       cache.set(`movie${query}`, result, cacheExpiry);
     }
-    console.log(cache.keys());
-    const cachedResults = cache.get(`movie${query}`);
     return res.json(cachedResults || result);
   } catch (error) {
     res.status(500).json({ error: "Error fetching data" });
     console.log("error fetching data", error);
+  }
+});
+app.post("/add-movie", async (req, res) => {
+  try {
+    const db = await getDb();
+    const movie = req.body;
+    console.log("movie", movie);
+    await db.collection("movie_details").insertOne(movie);
+    res.status(200).send("OK");
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+app.post("/review", async (req, res) => {
+  try {
+    const db = await getDb();
+    const movieId = parseInt(req.query.movieId);
+    const user = parseInt(req.query.uid);
+    const likes = parseInt(req.query.likes);
+    const text = decodeURIComponent(req.query.text);
+    if (!movieId || !user || !likes || !text) {
+      res.status(400).send("no sufficient parameters");
+    }
+    const review = {
+      movieId,
+      user,
+      text,
+      likes,
+      date: new Date(),
+    };
+    await db.collection("reviews").insertOne(review);
+    res.status(201).send("success");
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+app.post("/like-review/:id", async (req, res) => {
+  try {
+    const db = await getDb();
+    const id = req.params.id;
+    if (!id) {
+      res.status(400).send("no sufficient parameters");
+    }
+    const result = await db
+      .collection("reviews")
+      .findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $inc: { likes: 1 } },
+        { returnOriginal: false }
+      );
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+app.post("/edit-review/:id", async (req, res) => {
+  try {
+    const db = await getDb();
+    const id = req.params.id;
+    const newText = req.body.text;
+    if (!id) {
+      res.status(400).send("no sufficient parameters");
+    }
+    const result = await db
+      .collection("reviews")
+      .findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { text: newText } },
+        { returnOriginal: false }
+      );
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+app.post("/set-top-movie", async (req, res) => {
+  try {
+    const order = req.body;
+    const db = await getDb();
+    console.log(order.numbers);
+    if (!order) {
+      res.status(400).send("No order provided");
+      return;
+    }
+    await db
+      .collection("top5")
+      .updateOne({}, { $set: { top5: order.numbers } });
+    res.status(200).send("OK");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.get("/my-reviews/:id", async (req, res) => {
+  try {
+    const db = await getDb();
+    const id = req.params.id;
+    const result = await db
+      .collection("reviews")
+      .find({ user: parseInt(id) })
+      .toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
